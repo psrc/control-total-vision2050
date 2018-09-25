688d9e40b107a30e81aba4f12623b43ef96377eb
Hana Sevcikova <hanas@uw.edu>
2018-09-25 12:55:54 -0700

    reset paths

--- Create_STC_2050_4model_NewRG.R
+++ Create_STC_2050_4model_NewRG.R
@@ -4,15 +4,15 @@ library(sqldf)
 # Where is this script
 script.dir <- 'J:/Projects/V2050/STC_RGS/Script'
 #script.dir <- '/Volumes/DataTeam/Projects/V2050/STC_RGS/Script' # for Hana
-script.dir <- '/Users/hana/psrc/R/control-total-vision2050' # for Hana
+#script.dir <- '/Users/hana/psrc/R/control-total-vision2050' # for Hana
 
 # Where do the data tables live
 data.dir <- script.dir
-data.dir <- file.path(script.dir, "data") # for Hana
+#data.dir <- file.path(script.dir, "data") # for Hana
 
 # Where does the Juris_Reporting file live
 juris.dir <- "J:/Projects/V2050/SEIS/Data_Support/script_input"
-juris.dir <- data.dir # for Hana
+#juris.dir <- data.dir # for Hana
 
 # Which RG col to use
 RGcol <- "RGID_Proposed"
